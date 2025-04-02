2343ae305803bf6143a2e26a73e63c06175249e6
Inchino <alessandro.incalza22@gmail.com>
2025-04-02 12:08:35 +0200

    Assembly 2

--- ProjectBW5/ProjectBW5/obj/Debug/net8.0/ProjectBW5.AssemblyInfo.cs
+++ ProjectBW5/ProjectBW5/obj/Debug/net8.0/ProjectBW5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879d6d6926d98c1d48ee9286ae5a208c2594c04d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51d00c997d791f8c126a90317d4482280662f414")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
