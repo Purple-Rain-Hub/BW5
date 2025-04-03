1f72afa43cc2e3c4110d0dbc21f53a3eea8deeec
Inchino <alessandro.incalza22@gmail.com>
2025-04-03 15:33:41 +0200

    Navbar

--- FrontEndBW5/src/App.jsx
+++ FrontEndBW5/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
-import "node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./components/MyNavbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyNavbar/>
+    </>
+  );
 }
 
 export default App;
--- ProjectBW5/ProjectBW5/obj/Debug/net8.0/ProjectBW5.AssemblyInfo.cs
+++ ProjectBW5/ProjectBW5/obj/Debug/net8.0/ProjectBW5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc89d246ad731985489dc50fe4b56ef2d4874a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dfb1d458d75a189bc6af74a259f9cd9e0ec410c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectBW5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
